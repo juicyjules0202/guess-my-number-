69e858f8307a126df09ffaa79e529021943cdba1
student <student@SSCs-iMac-3.local>
2018-10-29 09:57:31 +0800

    first commit afer jhonny wrecks project

--- guess my number!/ViewController.swift
+++ guess my number!/ViewController.swift
@@ -2,14 +2,21 @@
 //  ViewController.swift
 //  guess my number!
 //
-//  Created by student on 29/10/2018.
-//  Copyright © 2018 student. All rights reserved.
+//  Created by dogeFatBoii on 29/10/2018.
+//  Copyright © 2018 dogeFatBoii. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    //defining UI
+    
+    @IBOutlet weak var lblMain: UILabel!
+    @IBOutlet weak var lblMin: UILabel!
+    @IBOutlet weak var lblMax: UILabel!
+    @IBOutlet weak var btnConfirmation: UIButton!
+    @IBOutlet weak var sliderForNumber: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
