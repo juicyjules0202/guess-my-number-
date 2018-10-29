3db44e64c487bc919782fd873443f81738f1f48c
student <student@sscs-imac-3.local>
2018-10-29 08:58:51 +0800

    Initial Commit

---
+++ guess my number!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  guess my number!
+//
+//  Created by student on 29/10/2018.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ guess my number!Tests/guess_my_number_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  guess_my_number_Tests.swift
+//  guess my number!Tests
+//
+//  Created by student on 29/10/2018.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+import XCTest
+@testable import guess_my_number_
+
+class guess_my_number_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
