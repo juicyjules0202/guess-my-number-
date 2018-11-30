0e33e6bb9610beff39683491a52109e1a51f8e87
student <student@cpe-172-100-19-37.twcny.res.rr.com>
2018-11-30 09:39:35 +0800

    despacito donepacito

--- guess my number!/ViewController.swift
+++ guess my number!/ViewController.swift
@@ -13,9 +13,11 @@ import UIKit
     var guess = 50
     var answerToRound = Int(arc4random_uniform(100)) + 1
     var guessesLeft = 7
+    var originalGuesses = 7
 class ViewController: UIViewController {
     //defining UI
     
+    @IBOutlet weak var btnRESET: UIButton!
     @IBOutlet weak var lblGuesses: UILabel!
     @IBOutlet weak var lblMain: UILabel!
     @IBOutlet weak var lblMin: UILabel!
@@ -27,13 +29,15 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        answerToRound = Int(arc4random_uniform(100)) + 1
         sliderForNumber.minimumValue = Float(Int(minimumGuess))
         sliderForNumber.maximumValue = Float(Int(maximumGuess))
         lblMain.text = "Guess the number!"
-        lblGuess.text = "\(guess)"
+        lblGuess.text = "your guess is:\(guess)"
         lblMax.text = "\(maximumGuess)"
         lblMin.text = "\(minimumGuess)"
         print(answerToRound)
+        lblGuesses.text = "you have \(guessesLeft) guesses left!"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -42,24 +46,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
    
+
+    @IBAction func actReset(_ sender: Any) {
+        minimumGuess = 1
+        maximumGuess = 100
+        guessesLeft = originalGuesses
+        sliderForNumber.minimumValue = Float(Int(minimumGuess))
+        sliderForNumber.maximumValue = Float(Int(maximumGuess))
+        lblMain.text = "Dont you get bored?"
+        lblGuess.text = "your guess is:\(guess)"
+        lblMax.text = "\(maximumGuess)"
+        lblMin.text = "\(minimumGuess)"
+        print(answerToRound)
+        lblGuesses.text = "you have \(guessesLeft) guesses left!"
+    }
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         guess = Int(sender.value)
-        lblGuess.text = "\(guess)"
+        lblGuess.text = "your guess is:\(guess)"
     }
     func commonFiles() {
         if guessesLeft == 2 {
             lblMain.text = "last chance left!"
         } else if guessesLeft == 1{
+            if guess == answerToRound{
+                lblMain.text = "near miss bro"
+            }else{
             lblMain.text = "you died apparently ;-;"
+            }
         }
         guessesLeft -= 1
         sliderForNumber.minimumValue = Float(minimumGuess)
         sliderForNumber.maximumValue = Float(maximumGuess)
+        lblGuesses.text = "you have \(guessesLeft) guesses left!"
     }
     @IBAction func executeGuess(_ sender: Any) {
         if minimumGuess != maximumGuess && guessesLeft != 0{
             if guess == answerToRound{
-                lblMain.text = "you won in \(guessesLeft) guesses!"
+                lblMain.text = "you won in \(originalGuesses - guessesLeft + 1) guesses!"
             } else if guess < answerToRound{
                 lblMain.text = "No, not yet there..."
                 minimumGuess = guess + 1
